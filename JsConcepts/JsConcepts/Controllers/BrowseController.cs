64a0fd329ff2b3342275b03ae98656c71a10a8e0
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2013-05-03 13:14:02 +0300

    Partial commit: Browse view/controller

---
+++ JsConcepts/JsConcepts/Controllers/BrowseController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model;
+using Model.Projections;
+
+namespace JsConcepts.Controllers
+{
+    public class BrowseController : BaseController
+    {
+        //
+        // GET: /Browse/
+
+        public ActionResult Index()
+        {
+            return View(Store.FetchAll<Contact>().AsContactBriefInfo()
+                .OrderBy(c => c.FirstName)
+                .ThenBy(c => c.LastName));
+        }
+
+    }
+}
